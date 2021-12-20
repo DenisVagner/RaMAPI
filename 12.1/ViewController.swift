16a8d8e11483f1c4481161beeaf0010f3aae8ccb
Denis <vagnerdenis26@gmail.com>
2021-12-20 22:38:03 +0300

    pagination added

--- 12.1/ViewController.swift
+++ 12.1/ViewController.swift
@@ -6,9 +6,8 @@ import Alamofire
 class ViewController: UIViewController {
     
     @IBOutlet weak var myTableView: UITableView!
-        
+    
     var result: AllCharacters? = nil
-    //var nextPageData: AllCharacters? = nil
     var currentPage = 1
     var urlString: String {
         get {
@@ -36,7 +35,7 @@ class ViewController: UIViewController {
         }
     }
     
-    // Запрос данных о первых 20 персонажей
+    // Запрос данных о 20 персонажах
     func loadFromNetworkAF(url: String) {
         networkRequestAF.requestAll(urlString: urlString) { [weak self] result in
             switch result {
@@ -55,25 +54,25 @@ class ViewController: UIViewController {
         }
     }
     
-//    func alertPresent() {
-//        let ac = UIAlertController(title: "Error", message: "No inernet", preferredStyle: .alert)
-//        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
-//        ac.addAction(cancelAction)
-//        present(ac, animated: true, completion: nil)
-//    }
+    //    func alertPresent() {
+    //        let ac = UIAlertController(title: "Error", message: "No inernet", preferredStyle: .alert)
+    //        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+    //        ac.addAction(cancelAction)
+    //        present(ac, animated: true, completion: nil)
+    //    }
     
-//    func loadFromNetworkNextPage(url: String) {
-//        networkRequestAF.requestAll(urlString: urlString) { [weak self] result in
-//            switch result {
-//
-//            case .success(let allcharacters):
-//                self?.nextPageData = allcharacters
-//                self?.myTableView.reloadData()
-//            case .failure(let error):
-//                print(error)
-//            }
-//        }
-//    }
+    //    func loadFromNetworkNextPage(url: String) {
+    //        networkRequestAF.requestAll(urlString: urlString) { [weak self] result in
+    //            switch result {
+    //
+    //            case .success(let allcharacters):
+    //                self?.nextPageData = allcharacters
+    //                self?.myTableView.reloadData()
+    //            case .failure(let error):
+    //                print(error)
+    //            }
+    //        }
+    //    }
 }
 
 // экстеншены для работы с таблицей
@@ -81,52 +80,51 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
     // передеча значения количества ячеек в таблице
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return result?.results.count  ?? 3
+        return result?.results.count  ?? 0
     }
     
     // заполнение ячеек данными, полученными ранее в результате запроса
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! CharacterInfoTableViewCell
+        
         cell.chatacterNameLabel.text = result?.results[indexPath.row].name
         cell.characterStatusLabel.text = result?.results[indexPath.row].status
         cell.characterLastSeenLocLabel.text = result?.results[indexPath.row].location.name
         
-        // вывод катринки персонажа
-        if let urlString8 = self.result?.results[indexPath.row].image {
+        // Пагинация
+        if (indexPath.row + 1) == result?.results.count {
+            currentPage += 1
+            DispatchQueue.global(qos: .utility).async {
+                self.loadFromNetworkAF(url: self.urlString)
+            }
+        }
+        // вывод картинки персонажа
+        if let urlStringImage = self.result?.results[indexPath.row].image {
             cell.activityIndicatorInCell.startAnimating()
+            
             // проверка на наличие изображения в кэше, если есть - грузить с него, если нет, то загружать картинку, выводить и затем помещать в кэш
-            if let image = self.imageCache.object(forKey: urlString8 as AnyObject) as? UIImage {
+            if let image = self.imageCache.object(forKey: urlStringImage as AnyObject) as? UIImage {
                 cell.characterImage.image = image
                 print("Image \(indexPath.row + 1) loaded from cache")
                 cell.activityIndicatorInCell.stopAnimating()
             } else {
-                let urlImage = URL(string: urlString8)
+                let urlImage = URL(string: urlStringImage)
                 let queue = DispatchQueue.global(qos: .utility)
                 queue.async {
                     if let imageData = try? Data(contentsOf: urlImage!) {
+                        let image = UIImage(data: imageData)
                         DispatchQueue.main.async {
-                            
-                            let image = UIImage(data: imageData)
                             cell.characterImage.image = image
-                            self.imageCache.setObject(image!, forKey: urlString8 as AnyObject)
+                            self.imageCache.setObject(image!, forKey: urlStringImage as AnyObject)
                             print("Image \(indexPath.row + 1) downloaded")
                             cell.activityIndicatorInCell.stopAnimating()
-                            //print("next page: ", self.result?.info.next)
                         }
                     }
-                    
                 }
             }
         }
         return cell
     }
-    
-//    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-//        if indexPath.row < (result?.results.count)! {
-//            loadFromNetworkNextPage(url: (result?.info.next)!)
-//            self.result?.results.append(contentsOf: nextPageData!.results)
-//        }
-//    }
     
     // убирает выделение ячейки
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
